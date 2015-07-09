080fcd8a01a2f046aedc2089f34c3008d5ef03a8
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-09 13:23:25 -0500

    fdasd

--- TwoUILabelsOverlay/ViewController.h
+++ TwoUILabelsOverlay/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *one;
+@property (weak, nonatomic) IBOutlet UILabel *two;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
 
 
 @end
--- TwoUILabelsOverlay/ViewController.m
+++ TwoUILabelsOverlay/ViewController.m
@@ -16,6 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    CGRect frame = CGRectMake(50, 50, 100, 100);
+    
+    UILabel *one = [[UILabel alloc]initWithFrame:frame];
+    [one setFont:[UIFont fontWithName:@"person" size:50]];
+    [one setText:@"m"];
+    one.textColor = [UIColor grayColor];
+//    self.one.alpha = 0.5;
+     CGRect frame2 = CGRectMake(50, 50, 95, 95);
+    UILabel *two = [[UILabel alloc]initWithFrame:frame2] ;
+    
+    [two setFont:[UIFont fontWithName:@"person" size:40]];
+    [two setText:@"C"];
+    two.textColor = [UIColor grayColor];
+//    two.alpha = 0.5;
+    [self.view addSubview:one];
+    [self.view addSubview:two];
+    
+
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
