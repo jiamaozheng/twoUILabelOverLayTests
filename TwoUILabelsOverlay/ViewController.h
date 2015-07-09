e1be56967a2cf6f49a70970e10ba94f2c44a5e8f
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-09 12:31:28 -0500

    Initial Commit

---
+++ TwoUILabelsOverlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwoUILabelsOverlay
+//
+//  Created by Jiamao Zheng on 7/9/15.
+//  Copyright (c) 2015 Emerge Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TwoUILabelsOverlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TwoUILabelsOverlay
+//
+//  Created by Jiamao Zheng on 7/9/15.
+//  Copyright (c) 2015 Emerge Media. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
